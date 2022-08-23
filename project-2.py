855386dea05da48d6605f6f2f99b063ee549d92c
Vokee Net <20075089@H2WHK0Y7Q6NW.tld>
2022-08-23 12:58:06 -0500

    part1

---
+++ project-2.py
@@ -0,0 +1,14 @@
+import os
+from os import system
+system("clear")
+
+import random
+
+# wordbank = ['Crapulous' , 'Grumpish' , 'Groak' , 'Pismire' , 'Snowbrowth' , 'Excogigate' , 'Apricity' , 'Twattle' ,'Elflock' , ' Gorgonize' , ' Cockalorum ', 'Snoutfair' ,  'Jollux' ­, 'Curglaff' , 'Brabble' , 'Twitter-light' , 'Lunting ' , 'Beef-witted' , 'Monsterful' , 'Callipygian ' , 'Fuzzle ' , 'Quockerwodger ' , 'Resistentialism' , 'Lethophobia ' , 'Sluberdegullion' , 'Curmuring' , 'Lumming ' , 'California widow ' ,  'Zenzizenzizenzic' , 'Houppelande']
+
+wordbank = ['Grumpish', 'Groak', 'Pismire' , 'Crapulous' , 'Snowbrowth' , 'Excogigate' , 'Apricity ' , 'Twattle' , 'Elflock' , 'Gorgonize' , 'Cockalorum' , 'Snoutfair' ,'Jollux' , 'Curglaff' , 'Brabble' , 'Twitter-light' ,'Lunting' ,'Beef-witted' ,'Monsterful' ,'Callipygian' , 'Fuzzle' ,'Quockerwodger' , 'Resistentialism' , 'Lethophobia' , 'Sluberdegullion' , 'Curmuring' , 'Lumming' ,'California widow' ,'Zenzizenzizenzic' ,'Houppelande']
+word = random.choice(wordbank)
+
+print(word)
+
+
